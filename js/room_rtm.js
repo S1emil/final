5e10ac57f00b30b765f541bf5a4865ff5e08f1e3
S1emil <48591106+S1emil@users.noreply.github.com>
2024-06-25 10:26:20 +0300

    Update room_rtm.js

--- js/room_rtm.js
+++ js/room_rtm.js
@@ -103,7 +103,7 @@ let addBotMessageToDom = (botMessage) => {
 
     let newMessage = `<div class="message__wrapper">
                         <div class="message__body__bot">
-                            <strong class="message__author__bot">🤖 Mumble Bot</strong>
+                            <strong class="message__author__bot">🤖 Kos Bot</strong>
                             <p class="message__text__bot">${botMessage}</p>
                         </div>
                     </div>`
